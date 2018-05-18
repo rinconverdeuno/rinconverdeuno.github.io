86ce554ff4e5e4dd93345397128e5e1f46f6f53b
Alberto Mesen <Alberto Mesen>
2018-05-18 17:52:00 -0600

    improved redirect

---
+++ js/misc.js
@@ -0,0 +1,8 @@
+
+
+
+function performSyncronousRequest() {
+	var redirectWindow = window.open('https://docs.google.com/forms/d/e/1FAIpQLScjvWeencXZCK-aVqOggXbmu4cjH-6DT3CRyir3Uafxi6hLOg/viewform?c=0&w=1&usp=mail_form_link', '_blank');
+    redirectWindow.location;
+}
+
